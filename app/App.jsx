f31a110fd12482b72f8e54e4889c314422d27b00
ernie <ernie@epicbytes.net>
2016-12-23 02:16:18 +0100

    derp

--- app/App.jsx
+++ app/App.jsx
@@ -29,38 +29,6 @@ class App extends React.Component {
     sr.reveal('.fadeInLeft, .bounceInLeft', { delay: 250, origin: 'right' });
     sr.reveal('.fadeIn', { delay: 250 });
     SmoothScroll.init({ updateURL: false });
-    window.initMap = () => {
-      const point = {
-        lat: 59.899519,
-        lng: 10.6287
-      };
-
-      const customMapType = new google.maps.StyledMapType([{ // eslint-disable-line no-undef
-        stylers: [{
-          saturation: -100
-        }]
-      }]);
-      const customMapTypeId = 'appyard';
-
-      const map = new google.maps.Map(document.getElementById('Map'), { // eslint-disable-line no-undef
-        center: point,
-        zoom: 12,
-        disableDefaultUI: true,
-        scrollwheel: false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        draggable: false
-      });
-
-      const marker = new google.maps.Marker({ // eslint-disable-line no-undef, no-unused-vars
-        position: point,
-        map
-      });
-
-      map.mapTypes.set(customMapTypeId, customMapType);
-      map.setMapTypeId(customMapTypeId);
-    };
   }
   render() {
     const notificationClass = classNames({
